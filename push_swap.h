2e352be7307b911e144971c15aba169192056d0e
Dejitaru <joao.mota280396@gmail.com>
2025-01-19 16:16:11 +0000

    trying something

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jomanuel <jomanuel@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 12:42:36 by jomanuel          #+#    #+#             */
-/*   Updated: 2025/01/19 14:41:29 by jomanuel         ###   ########.fr       */
+/*   Updated: 2025/01/19 16:01:49 by jomanuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,15 @@
 # include <limits.h>
 # include <stddef.h>
 # include <stdint.h>
+# include "libft/libft.h"
 
 typedef	struct s_list
 {
-	int				number;
+	int				content;
 	int				index;
 	struct s_list	*next;
+	struct s_list	*prev;
+	
 }	t_list;
 
 int		is_valid_arg(char *str);
